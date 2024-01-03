a3e53587d9fe7cc5b001429c6c046c495e56a790
Bernardo Rodrigues da Silva <bernardo.rodsilva@gmail.com>
2024-01-02 23:26:17 -0300

    refactoring

--- Top Interview 150 - Solves/88.py
+++ Top Interview 150 - Solves/88.py
@@ -2,15 +2,12 @@
 
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        nums1_pointer = m - 1
-        nums2_pointer = n - 1
-
-        merged_pointer = m + n - 1
-        while merged_pointer >= 0:
-            if nums1_pointer >= 0 and (nums2_pointer < 0 or nums1[nums1_pointer] > nums2[nums2_pointer]):
-                nums1[merged_pointer] = nums1[nums1_pointer]
-                nums1_pointer -= 1
+        p1, p2, i = m - 1, n - 1, m + n - 1
+        while p2 >= 0:
+            if p1 >= 0 and nums1[p1] > nums2[p2]:
+                nums1[i] = nums1[p1]
+                p1 -= 1
             else:
-                nums1[merged_pointer] = nums2[nums2_pointer]
-                nums2_pointer -= 1
-            merged_pointer -= 1
+                nums1[i] = nums2[p2]
+                p2 -= 1
+            i -= 1
